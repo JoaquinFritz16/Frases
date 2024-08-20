17f9b9761be09c3f5e8f6c21aff72522ef1b96eb
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2024-08-19 22:45:15 -0300

    Terminado

--- Frases/src/App.jsx
+++ Frases/src/App.jsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
-
+import './App.css'
 function App() {
-	const [frase, setfrase] = useState("");
-	const getfrases = () => {
-		fetch("https://uselessfacts.jsph.pl/")
+	const [facts, setFacts] = useState("");
+	const getFacts = () => {
+		fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
 			.then((response) => response.json())
-			.then((data) => setfrase(data.quote))
+			.then((data) => setFacts(data.text))
 			.catch((error) => console.error("Error fetching data:", error));
 	};
-	useEffect(getfrases, []);
+	useEffect(getFacts, []);
 	return (
-		<div>
-			<h1>Frases Random</h1>
-			{frase && <p>frase random: {frase}</p>}
+		<div className="Contenedor_todo">
+			<h1>Random Facts</h1>
+			{facts && <p className="fact">A random fact: {facts}</p>}
 			<div>
-				<button onClick={getfrases}>Get frase</button>
+				<button onClick={getFacts}>Get a fact</button>
 			</div>
 		</div>
 	);
