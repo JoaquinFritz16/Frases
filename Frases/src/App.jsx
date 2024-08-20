8a60fc98615c3a7efefcd451a692a97c9880b927
JoaquinFritz16 <d48125437@alumnos.itr.edu.ar>
2024-08-19 22:57:04 -0300

    Manejo de errores

--- Frases/src/App.jsx
+++ Frases/src/App.jsx
@@ -2,17 +2,28 @@ import React, { useState, useEffect } from "react";
 import './App.css'
 function App() {
 	const [facts, setFacts] = useState("");
+	const [error, setError] = useState(null);
 	const getFacts = () => {
 		fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
-			.then((response) => response.json())
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`Error: ${response.status}`)
+				}
+				return response.json();
+			})
 			.then((data) => setFacts(data.text))
-			.catch((error) => console.error("Error fetching data:", error));
+			.catch((error) => {
+				setError(error.message);
+			});
 	};
 	useEffect(getFacts, []);
 	return (
 		<div className="Contenedor_todo">
 			<h1>Random Facts</h1>
-			{facts && <p className="fact">A random fact: {facts}</p>}
+			{error? (
+				<p>{error}</p>
+			):(
+			facts && <p className="fact">A random fact: {facts}</p>)}
 			<div>
 				<button onClick={getFacts}>Get a fact</button>
 			</div>
